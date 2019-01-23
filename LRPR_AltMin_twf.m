0f94fce73a86b5a69d9a5d6d9a3c5241aac7775d
Praneeth Narayanamurthy <praneeth.kurpad@gmail.com>
2019-01-23 10:01:39 -0600

    update:

--- LRPR_AltMin_twf.m
+++ LRPR_AltMin_twf.m
@@ -1,9 +1,9 @@
 %%  Low Rank Phase Retrieval using Alternating Minimization
-function [Xhat, Uhat, U_track, X_track, time_iter] = LRPR_AltMin_twf(Y, A, Params)
+function [Xhat, Uhat, U_track, X_track, time_iter] = LRPR_AltMin_twf(Y, A, Params, Paramstwf)
 
 time_iter = zeros(Params.told, 1);
 Xhat    =   zeros(Params.n, Params.q);
-
+ctr = 1;
 %tic;
 %%   Initialization
 Yu  =   zeros(Params.n, Params.n);
@@ -30,6 +30,9 @@ if(Params.rank_est_flag == 1) %%need to estimate rank
 end
 [Ui, ~, ~]  =   svd(Yu);
 Uhat        =   Ui(:, 1:Params.r);
+U_track{ctr} = Uhat;
+
+
 Bhat        =   zeros(Params.r, Params.q);
 
 for k = 1 : Params.q
@@ -41,6 +44,8 @@ for k = 1 : Params.q
     Xhat(:,k)        =   Uhat * Bhat(:,k);
     
 end
+X_track{ctr} = Xhat;
+ctr = ctr+1;
 Chat    =   zeros(Params.m, Params.n);
 for a = 1 : Params.q
     Chat(:,a)   =   exp(1i*angle(A(:,:,a)' * Uhat * Bhat(:,a)));
@@ -48,60 +53,50 @@ end
 
 %time_iter(1) = toc;
 
-%%  Main Loop
-Ysqrt   =   sqrt(Y);
-Zvec    =   zeros(Params.m*Params.q, 1);
-tic;
-for t = 1 : Params.told
-    U_track{t} = Uhat;
-    X_track{t} = Uhat * Bhat;
-    %tic;
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    %       Updating Uhat
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    for a = 1 : Params.q
-        
-        strt_idx    =   Params.m*(a-1) + 1;
-        end_idx     =   strt_idx + Params.m - 1;
-        TempVec     =   Chat(:,a) .* Ysqrt(:,a);
-        Zvec(strt_idx:end_idx, 1)   =   TempVec;
-        
-    end
-    %     Uvec    =   cgls_new(@mult_H, @mult_Ht , Zvec, 0, 10^-14,20);
-    Uvec    =   cgls_new(@mult_H, @mult_Ht , Zvec, 0, 10^-16,30);
-    Uhat    =   reshape(Uvec, Params.n, Params.r);
-    
-    
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    %       Updating Bhat and Chat
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    for  a = 1 : Params.q
-        R           =   A(:,:,a)' * Uhat;
-        K           =   Chat(:,a) .* Ysqrt(:,a);
-        Bhat(:,a)   =   mldivide(R , K );
-        Chat(:,a)   =   exp(1i*angle(R * Bhat(:,a)));
-        Xhat(:,a)   =   Uhat * Bhat(:, a);
-        
-    end
-    time_iter(t) = toc;
+
+%%need to invoke TWF here
+
+Rel_Err = [];
+%% Loop
+grad_type = Paramstwf.grad_type;
+if strcmp(grad_type, 'TWF_Poiss') == 1
+    mu = @(t) Paramstwf.mu; % Schedule for step size
+elseif strcmp(grad_type, 'WF_Poiss') == 1
+    tau0 = 330;                         % Time constant for step size
+    mu = @(t) min(1-exp(-t/tau0), 0.2); % Schedule for step size
 end
 
-%%%% helper functions
-    function x_out = mult_H(x_in)
-        X_mat    =   reshape(x_in, Params.n, Params.r);
-        %    x_out    =   A_long * X_vec;
-        x_out    =   zeros(Params.q*Params.m, 1);
-        for na = 1: Params.q
-            x_out((na-1)*Params.m + 1 : na*Params.m) = A(:,:,na)' * X_mat * Bhat(:,na);
-        end
+for t = 1: Paramstwf.T
+    for a = 1 : Paramstwf.alpha
+        z = Xhat(:, a);
+        z              =   Uhat * Uhat' * z;
+        Aa      =   @(I) A(:,:,a) * I;
+        Aa_t    =   @(Y) A(:,:,a)' * Y;
+        grad(:,a) = compute_grad(z(:,a), Y(:,a), Paramstwf, Aa, Aa_t);
+        z(:,a) = z(:,a) - mu(t) * grad(:,a);             % Gradient update
+        % Relerrs2(a) = [norm(X(:,a) - exp(-1i*angle(trace(X(:,a)'*z(:,a)))) * z(:,a), 'fro')/norm(X(:,a),'fro')];
+        % Xtwf(:,a,t) = z(:,a);
+        
     end
-
-
-    function w_out = mult_Ht(w_in)
-        w_out   =   zeros(Params.n*Params.r, 1);
-        for na = 1: Params.q
-            tmp_vec  =   A(:,:,na) * w_in((na-1)*Params.m+1:na*Params.m);
-            w_out    =   w_out + kron(Bhat(:,na), tmp_vec);
-        end
+    % Xtwf(:,:,t)        =   Xtwf ;
+    [Utmp,~]    =   svd(z);
+    Uhat                =   Utmp(:,1:Paramstwf.r);
+    U_track{ctr} = Uhat;
+    ctr = ctr+1;
+    Xhat = Uhat * Uhat' * z;
+    %fprintf('TWF Projection Error is:\t%2.2e\n', mean(Tmp_Err_X));
+    for na = 1 : Paramstwf.alpha
+        xa_hat      =   z(:,na);
+        xa          =   X(:,na);
+        %                 %  Tmp_Err_X(na)   =   min(norm(xa-xa_hat)^2, norm(xa+xa_hat)^2);
+        Tmp_Err_X(na) 	=   norm(xa - exp(-1i*angle(trace(xa'*xa_hat))) * xa_hat, 'fro');
     end
+    Proj2_Err =  sum(Tmp_Err_X)/norm(X,'fro');
+    if (Proj2_Err <= Treshold)
+        return;
+    end
+end
+%           Rel_Err(:,t)=  Tmp_Err_X;
+%           fprintf('TWF Projection Error is:\t%2.2e\n', mean(Tmp_Err_X));
+
 end
